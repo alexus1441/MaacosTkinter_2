cb16ae05e0292acbc1c7fa47482ebdf8383e3ead
vplaudis2 <71326797+vplaudis2@users.noreply.github.com>
2021-09-21 13:36:10 +0300

    sheesh
    please take my application for the job

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ window = tk.Tk()
 sveiciens = tk.Label(text = "datoriku maacos")
 
 sveiciens.pack()
+button = tk.Button(
+  text="Nospied!",
+  width=25,
+  height=5,
+  bg="violet",
+  fg"dark green",
+)
+button.pack()
+window.mainloop()
